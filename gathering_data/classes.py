7d05f244b42981210ab2516f6efe8c51b04d4b29
Jae Young Suh <dddsaty09@naver.com>
2024-12-15 00:16:21 +0900

    Add files via upload

---
+++ gathering_data/classes.py
@@ -0,0 +1,359 @@
+from shapely.geometry import Point, Polygon
+import cv2
+import numpy as np
+
+
+# Con
+
+def LIST_EXTENDS(v1: list, v2: list):
+    v1.extend(v2)
+    return v1
+
+
+class NLocation:
+    TO_INTEGER = 10 ** 8
+
+    def __init__(self, lat, lon, zoom=16):
+        self.lat = lat if type(lat) == 'float' else float(lat)
+        self.lon = lon if type(lon) == 'float' else float(lon)
+        self.zoom = zoom
+
+    def get_around_param(self):
+        return {
+            'leftLon': self.lon - 0.0137329,
+            'rightLon': self.lon + 0.0137329,
+            'topLat': self.lat + 0.0069786,
+            'bottomLat': self.lat - 0.0069786
+        }
+
+    def get_tuple(self):
+        return (self.lat, self.lon)
+
+    def __str__(self) -> str:
+        return "loc(%f | %f)" % (self.lat, self.lon)
+
+
+class NMap:
+    def __init__(self, shape_vertexs=[]) -> None:
+        self.vertexs = []
+        for vs in shape_vertexs:
+            if len(vs) == 0: continue
+            self.vertexs.append(vs)
+        self.polys = [Polygon(vs) for vs in self.vertexs]
+
+    def contain(self, loc: NLocation):
+        p = Point(loc.lat, loc.lon)
+        for poly in self.polys:
+            if p.within(poly) is True:
+                return True
+        return False
+
+    def get_dimension(self):
+        return NDimension(self.vertexs)
+
+
+class NDust:
+    def __init__(self, tag, loc) -> None:
+        self.tag = tag
+        self.lat, self.lon = loc[0], loc[1]
+
+
+class NDimension:
+    RESOLUTION = np.array([500, 500])
+    PADDING = np.array([6, 6])
+    LINE_WIDTH = 5
+
+    def __init__(self, vertexs: list) -> None:
+        self.x_scale, self.y_scale = NDimension.get_scale(vertexs)
+        self.outlines = [NDimension.fit_scale_with_split(v, (self.x_scale, self.y_scale)) for v in vertexs]
+
+    def get_img(self, data: list[NDust] = [], tag_color={}):
+        img = self.get_bg_img()
+
+        if len(data) == 0: return img
+        return NDimension.dot_vertexs(img, data, tag_color)
+
+    def get_bg_img(self):
+        size = NDimension.RESOLUTION + 2 * NDimension.PADDING
+        img = np.zeros((size[0], size[1], 3), dtype=np.uint8) + 255
+        for ol in self.outlines:
+            img = NDimension.draw_vertexs(img, ol)
+        return img
+
+    @classmethod
+    def draw_vertexs(self, img, vertexs):
+        img = cv2.line(img, vertexs[0], vertexs[-1], (0, 0, 0), NDimension.LINE_WIDTH)
+        for i in range(len(vertexs) - 1):
+            img = cv2.line(img, vertexs[i], vertexs[i + 1], (0, 0, 0), NDimension.LINE_WIDTH)
+        return img
+
+    @classmethod
+    def dot_vertexs(self, img, vertexs: list[NDust], tag_color):
+        for v in vertexs:
+            cv2.circle(img, (v.lat, v.lon), NDimension.LINE_WIDTH, tag_color[v.tag], -1)
+        return img
+
+    @classmethod
+    def fit_scale_with_split(cls, list, scale):
+        x, y = cls.split_x_y(list)
+        return np.column_stack((
+            cls.fit_scale(x, scale[0]),
+            cls.fit_scale(y, scale[1], 1)
+        ))
+
+    @classmethod
+    def transform_type(cls, value):
+        if type(value) == np.ndarray:
+            return value
+        if type(value) == list:
+            return np.array(value)
+        return value
+
+    @classmethod
+    def to_integer(cls, value):
+        if type(value) == np.ndarray:
+            return value.astype(int)
+        if type(value) == list:
+            return np.array(value).astype(int)
+        return int(value)
+
+    @classmethod
+    def fit_scale(cls, var, scale, axis=0):
+        var = cls.transform_type(cls.upper(var))
+        scaled = ((var - scale[0]) * NDimension.RESOLUTION[axis] // scale[1]) + NDimension.PADDING[axis]
+        return cls.to_integer(scaled)
+
+    @classmethod
+    def get_scale(cls, vertexs):
+        union = []
+        for vl in vertexs: union.extend(vl)
+        x, y = cls.split_x_y(cls.upper(union))
+        mnX, mnY = min(x), min(y)
+        return (mnX, max(x) - mnX), (mnY, max(y) - mnY)
+
+    @classmethod
+    def upper(cls, var):
+        return cls.transform_type(var) * NLocation.TO_INTEGER
+
+    @classmethod
+    def split_x_y(cls, var):
+        if type(var) == list:
+            var = np.array(var)
+        return var[:, 0], var[:, 1]
+
+    @classmethod
+    def get_default_tag_color(cls):
+        return {
+            'APT': (0, 255, 0),  # 아파트
+            'ABYG': (0, 255, 0),  # 아파트 분양권
+
+            'OPST': (255, 0, 0),  # 오피스텔
+            'OBYG': (255, 0, 0),  # 오피스텔 분양권
+
+            'JGB': (0, 0, 255),  # 재개발
+            'JGC': (0, 0, 255),  # 재건축
+
+            'BUS': (0, 255, 255),  # 버스정류장
+            'METRO': (0, 255, 255),  # 지하철
+
+            'INFANT': (255, 255, 0),  # 어린이집
+            'PRESCHOOL': (255, 255, 0),  # 유치원
+
+            'PRI_SCHOOL': (0, 0, 0),
+            'PUB_SCHOOL': (0, 0, 0),
+            'HOSPITAL': (0, 0, 0),  # 병원
+            'PARKING': (0, 0, 0),  # 주차장
+            'MART': (0, 0, 0),  # 마트
+            'CONVENIENCE': (0, 0, 0),  # 편의점
+            'WASHING': (0, 0, 0),  # 세탁소
+            'BANK': (0, 0, 0),  # 은행
+            'OFFICE': (0, 0, 0)  # 관공서
+        }
+
+
+class NSector:
+    def __init__(self, name, loc, no, city, divisition, vertex) -> None:
+        self.divisition = divisition
+        self.city = city
+        self.name = name
+        self.loc = loc  # type: NLocation
+        self.no = no
+        self.map = NMap(vertex)
+
+    def get_param(self):
+        around = self.loc.get_around_param()
+        around.update({'cortarNo': self.no})
+        return around
+
+    def __str__(self) -> str:
+        return "%s %s %s %s %s" % (self.city, self.divisition, self.name, self.no, self.loc)
+
+
+class NRE_ROUTER:
+    REGION_LIST = 'regions/list'
+    CORTARS = 'cortars'
+    COMPLEX2 = 'complexes/single-markers/2.0'
+    NEIGHBORHOOD = 'regions/neighborhoods'
+    SCHOOL = 'schools'
+
+
+class NNeighborAround:
+    HEADER = ['BUS', 'METRO', 'INFANT', 'PRESCHOOL', 'HOSPITAL',
+              'PARKING', 'MART', 'CONVENIENCE', 'WASHING', 'BANK', 'OFFICE',
+              'PRI_SCHOOL', 'PUB_SCHOOL']
+
+    def __init__(self) -> None:
+        self.counter = {
+            'BUS': 0,
+            'METRO': 0,
+            'INFANT': 0,
+            'PRESCHOOL': 0,
+            'HOSPITAL': 0,
+            'PARKING': 0,
+            'MART': 0,
+            'CONVENIENCE': 0,
+            'WASHING': 0,
+            'BANK': 0,
+            'OFFICE': 0,
+            'PRI_SCHOOL': 0,
+            'PUB_SCHOOL': 0
+        }
+
+    def increase(self, tag=''):
+        self.counter[tag] += 1
+
+    def get_list(self):
+        return self.counter.values()
+
+
+class NNeighbor:
+    BUS = 'BUS'  # 버스정류장
+    METRO = 'METRO'  # 지하철
+    KID = 'INFANT'  # 어린이집
+    PRESCHOOL = 'PRESCHOOL'  # 유치원
+    SCHOOL = 'SCHOOLPOI'  # 학교
+    HOSPITAL = 'HOSPITAL'  # 병원
+    PARKING = 'PARKING'  # 주차장
+    MART = 'MART'  # 마트
+    CONVENIENCE = 'CONVENIENCE'  # 편의점
+    WASHING = 'WASHING'  # 세탁소
+    BANK = 'BANK'  # 은행
+    OFFICE = 'OFFICE'  # 관공서
+
+    EACH = [BUS, METRO, KID, PRESCHOOL, SCHOOL, HOSPITAL, PARKING, MART, CONVENIENCE, WASHING, BANK, OFFICE]
+
+    def __init__(self, type, name, loc) -> None:
+        self.type = type
+        self.name = name
+        self.loc = loc  # type: NLocation
+
+    def __str__(self) -> str:
+        return "%s %s %s" % (self.type, self.name, self.loc)
+
+
+class NArea:
+    def __init__(self, mn, mx, representative, floorRatio) -> None:
+        self.mn = mn
+        self.mx = mx
+        self.representative = representative
+        self.floorRatio = floorRatio
+
+
+class NPrice:
+    def __init__(self, mn, mx, med) -> None:
+        self.mn = mn if mn != 0 else None
+        self.mx = mx if mx != 0 else None
+        self.med = med if med != 0 else None
+
+    def __str__(self) -> str:
+        return "%f %f" % (self.mn, self.mx)
+
+
+class NThing:
+    HEADER = LIST_EXTENDS(['Name', 'Type', 'Build',
+                           'Dir', 'minArea', 'maxArea',
+                           'representativeArea', 'floorAreaRatio',
+                           'minDeal', 'maxDeal', 'medianDeal',
+                           'minLease', 'maxLease', 'medianLease',
+                           'minDealUnit', 'maxDealUnit', 'medianDealUnit',
+                           'minLeaseUnit', 'maxLeaseUnit', 'medianLeaseUnit',
+                           'Lat', 'Lon'],
+                          NNeighborAround.HEADER)
+
+    def __init__(self, name, type, buildTime, loc, area, deal, lease, udeal, ulease) -> None:
+        self.type = type
+        self.buildTime = buildTime
+        self.area = area  # type: NArea
+        self.name = name  # type: str
+        self.loc = loc  # type: NLocation
+        self.deal = deal  # type: NPrice
+        self.udeal = udeal  # type: NPrice
+        self.lease = lease  # type: NPrice
+        self.ulease = ulease  # type: NPrice
+        self.dir = ''
+        self.neiAround = NNeighborAround()
+
+    def get_list(self):
+        return LIST_EXTENDS(
+            [self.name, self.type, self.buildTime, self.dir, self.area.mn, self.area.mx, self.area.representative,
+             self.area.floorRatio,
+             self.deal.mn, self.deal.mx, self.deal.med, self.lease.mn, self.lease.mx, self.lease.med, self.udeal.mn,
+             self.udeal.mx, self.udeal.med, self.ulease.mn, self.ulease.mx, self.ulease.med, self.loc.lat,
+             self.loc.lon], self.neiAround.get_list())
+
+    def __str__(self) -> str:
+        return "%s %s %s" % (self.name, self.type, self.buildTime)
+
+
+class NRegion:
+    def __init__(self, name='', loc=None, no='') -> None:
+        self.name = name
+        self.loc = loc  # type: NLocation
+        self.no = no
+
+    def __str__(self) -> str:
+        return "%s %s %s" % (self.name, self.no, self.loc)
+
+
+class NAddon:
+    TRADE_DEAL = 'A1'  # 매매
+    TRADE_LEASE = 'B1'  # 전세
+    # 월세 : 미구현 Don't use it!
+    # TRADE_MON = 'B2'
+    ##단기 임대 : 미구현 Don't use it!
+    TRADE_SHO = 'B3'
+    ESTATE_APT = 'APT'  # 아파트
+    ESTATE_APT_AREA = 'ABYG'  # 아파트 분양권
+    ESTATE_APT_RESTRUCT = 'JGC'  # 재건축
+    ESTATE_OPST = 'OPST'  # 오피스텔
+    ESTATE_OPST_AREA = 'OBYG'  # 오피스텔 분양권
+    ESTATE_REMAKE = 'JGB'  # 재개발
+    DIR_EE = 'EE'  # 동
+    DIR_ES = 'ES'  # 남동
+    DIR_WW = 'WW'  # 서
+    DIR_WS = 'WS'  # 남서
+    DIR_SS = 'SS'  # 남
+    DIR_EN = 'EN'  # 북동
+    DIR_NN = 'NN'  # 북
+    DIR_WN = 'WN'  # 북서
+    DIR_EACH = [DIR_EE, DIR_ES, DIR_WW, DIR_WS, DIR_SS, DIR_EN, DIR_NN, DIR_WN]
+
+    def __init__(self, dir=[], tradeType=[], estateType=[]) -> None:
+        self.dir = dir
+        self.tradeType = tradeType
+        self.estateType = estateType
+
+    def get_param(self):
+        return {
+            'directions': self.preprocess(self.dir),
+            'tradeType': self.preprocess(self.tradeType),
+            'realEstateType': self.preprocess(self.estateType)
+        }
+
+    @classmethod
+    def preprocess(cls, value):
+        return value if type(value) == str else ':'.join(value)
+
+    @classmethod
+    def get_default(cls):
+        return NAddon([], [cls.TRADE_DEAL], [cls.ESTATE_APT])
---
+++ gathering_data/util.py
@@ -0,0 +1,290 @@
+from time import sleep
+from haversine import haversine
+from gathering_data.classes import *
+import requests
+
+BASE_API_URL = "https://new.land.naver.com/api/"
+# Check Log
+# Time
+IS_LOGGING = True
+
+
+def get(url="", params={}):
+    rep = requests.get(BASE_API_URL + url, params=params, headers={'User-Agent': '*'})
+    if IS_LOGGING is True: print('Get', rep.request.url)
+    if rep.status_code != 200: raise Exception('Response Error')
+    return rep.json()
+
+
+def get_neighborhood(sector: NSector, nType=''):
+    param = sector.loc.get_around_param()
+    param.update({'zoom': sector.loc.zoom})
+    res = None
+    if nType != NNeighbor.SCHOOL:
+        param.update({'type': nType})
+        res = get(NRE_ROUTER.NEIGHBORHOOD, param)
+    else:
+        res = get(NRE_ROUTER.SCHOOL, param)
+    return parse_neighbor(res, nType)
+
+
+def make_param_thing(sector: NSector, addon: NAddon = NAddon.get_default()):
+    param = {
+        'zoom': sector.loc.zoom,
+        'priceType': 'RETAIL',
+        'markerId': '',
+        'markerType': '',
+        'selectedComplexNo': '',
+        'selectedComplexBuildingNo': '',
+        'fakeComplexMarker': '',
+        'tag': '::::::::',
+        'rentPriceMin': 0,
+        'rentPriceMax': 900000000,
+        'priceMin': 0,
+        'priceMax': 900000000,
+        'areaMin': 0,
+        'areaMax': 900000000,
+        'oldBuildYears': '',
+        'recentlyBuildYears': '',
+        'minHouseHoldCount': '',
+        'maxHouseHoldCount': '',
+        'showArticle': True,
+        'sameAddressGroup': False,
+        'minMaintenanceCost': '',
+        'maxMaintenanceCost': '',
+    }
+    param.update(sector.get_param())
+    param.update(addon.get_param())
+    return param
+
+
+def get_things(sector: NSector, addon=NAddon.get_default()):
+    res = get(NRE_ROUTER.COMPLEX2, make_param_thing(sector, addon))
+    return parse_things(res, sector, addon.dir)
+
+
+def make_param_sector(loc: NLocation):
+    return {'centerLat': loc.lat, 'centerLon': loc.lon, 'zoom': loc.zoom}
+
+
+def get_sector(loc: NLocation):
+    res = get(NRE_ROUTER.CORTARS, make_param_sector(loc))
+    return parse_sector(res)
+
+
+def split_list(list: list, k: int = 5):
+    splited = []
+    step = len(list) // k
+    left = 0
+    end = step * k
+    while left < end:
+        splited.append(list[left: left + step])
+        left += step
+
+    if left <= len(list):
+        splited.append(list[left:])
+    return splited
+
+
+def default_loop(list):
+    return list
+
+
+def get_sector_list(regions: list[NRegion], delay: int = 2, interval: int = 10, loop=default_loop):
+    sectors = []
+    cancel = []
+    loading = 0
+    for reg in loop(regions):
+        try:
+            sector = get_sector(reg.loc)
+            sectors.append(sector)
+            loading += 1
+            if loading == interval:
+                sleep(delay)
+                loading = 0
+        except:
+            print("Error", reg)
+            cancel.append(reg)
+            get_sleep(20)
+            continue
+    return sectors, cancel
+
+
+def get_sleep(delay: int = 20):
+    sleep(delay)
+
+
+def make_param_region(code):
+    return {'cortarNo': code}
+
+
+def get_region_list(code="0000000000"):
+    res = get(NRE_ROUTER.REGION_LIST, make_param_region(code))
+    return parse_region(res)
+
+
+def parse_region(region_obj={}):
+    if len(region_obj) < 1:
+        return []
+    regions = []  # type: list[NRegion]
+    for obj in region_obj['regionList']:
+        regions.append(NRegion(
+            obj['cortarName'],
+            NLocation(obj['centerLat'], obj['centerLon']),
+            obj['cortarNo']))
+    return regions
+
+
+def parse_sector(sector_json: dict):
+    return NSector(sector_json['sectorName'], NLocation(sector_json['centerLat'], sector_json['centerLon']),
+                   sector_json['sectorNo'], sector_json['cityName'], sector_json['divisionName'],
+                   sector_json['cortarVertexLists'])
+
+
+def parse_neighbor(data, nType):
+    res = []  # type: list[NNeighbor]
+
+    if nType != NNeighbor.SCHOOL:
+        for v in data['neighborhoods']:
+            res.append(NNeighbor(
+                nType,
+                v['name'],
+                NLocation(v['latitude'], v['longitude'])
+            ))
+    else:
+        for v in data:
+            res.append(NNeighbor(
+                'PUB_SCHOOL' if v['organizationType'] == '공립' else 'PRI_SCHOOL',
+                v['schoolName'],
+                NLocation(v['latitude'], v['longitude']),
+            ))
+
+    if nType == NNeighbor.PRESCHOOL or nType == NNeighbor.KID:
+        res = filter_item(res, lambda x: len(x.name), lambda x, y: x.name in y.name and x.name != y.name)
+    return res
+
+
+def parse_things(results, sector: NSector, dir):
+    smap = sector.map
+    res = []  # type: list[NThing]
+    for v in results:
+        if 'minDealPrice' not in v and 'minLeasePrice' not in v:
+            continue
+        if v['dealCount'] == 0 and v['leaseCount'] == 0:
+            continue
+
+        thing = NThing(
+            v['complexName'],
+            v['realEstateTypeCode'],
+            v['completionYearMonth'],
+            NLocation(v['latitude'], v['longitude']),
+            NArea(v['minArea'], v['maxArea'], v['representativeArea'], v['floorAreaRatio']),
+            NPrice(v['minDealPrice'], v['maxDealPrice'], None if 'medianDealPrice' not in v else v['medianDealPrice']),
+            NPrice(v['minLeasePrice'], v['maxLeasePrice'],
+                   None if 'medianLeasePrice' not in v else v['medianLeasePrice']),
+            NPrice(v['minDealUnitPrice'], v['maxDealUnitPrice'],
+                   None if 'medianDealUnitPrice' not in v else v['medianDealUnitPrice']),
+            NPrice(v['minLeaseUnitPrice'], v['maxLeaseUnitPrice'],
+                   None if 'medianLeaseUnitPrice' not in v else v['medianLeaseUnitPrice'])
+        )
+
+        if smap.contain(thing.loc):
+            thing.dir = dir
+            res.append(thing)
+    return res
+
+
+def distance_between(l1: NLocation, l2: NLocation):
+    return round(haversine(l1.get_tuple(), l2.get_tuple(), unit='m'))
+
+
+def get_distance_standard(standard={}):
+    default_standard = {
+        'BUS': 500,
+        'METRO': 500,
+        'INFANT': 750,
+        'PRESCHOOL': 750,
+        'PRI_SCHOOL': 1000,
+        'PUB_SCHOOL': 1000,
+        'HOSPITAL': 2000,
+        'PARKING': 500,
+        'MART': 500,
+        'CONVENIENCE': 300,
+        'WASHING': 500,
+        'BANK': 750,
+        'OFFICE': 1250
+    }
+    default_standard.update(standard)
+    return default_standard
+
+
+def things_to_dusts(things: list[NThing], dimension: NDimension):
+    dusts = []
+    for t in things:
+        dusts.append(NDust(t.type, [
+            dimension.fit_scale(t.loc.lat, dimension.x_scale),
+            dimension.fit_scale(t.loc.lon, dimension.y_scale, 1)
+        ]))
+    return dusts
+
+
+def neighbors_to_dusts(neis: list[NNeighbor], dimension: NDimension):
+    dusts = []
+    for t in neis:
+        dusts.append(NDust(t.type, [
+            dimension.fit_scale(t.loc.lat, dimension.x_scale),
+            dimension.fit_scale(t.loc.lon, dimension.y_scale, 1)
+        ]))
+    return dusts
+
+
+def neighbor_prefix_flt(lhs: NNeighbor, rhs: NNeighbor):
+    return lhs.name in rhs.name
+
+
+def filter_item(list, to_key, condition):
+    items = sorted(list, key=to_key, reverse=True)
+    res = []
+    while len(items) > 0:
+        item = items.pop()
+        for it in items[:]:
+            if condition(item, it) is True:
+                items.remove(it)
+        res.append(item)
+    return res
+
+
+def update_things_intersection(things: list[NThing], neighbors: list[NNeighbor], standard):
+    for thing in things:  # 매물
+        around = NNeighborAround()
+        for nei in neighbors:  # 편의시설
+            d = distance_between(thing.loc, nei.loc)
+            if d <= standard[nei.type]:  # meter
+                around.increase(nei.type)
+        thing.neiAround = around
+
+
+def get_all_neighbors(sector):
+    neighbors = []  # 편의시설 기록
+    for nType in NNeighbor.EACH:  # 모든 편의시설
+        neighbors.extend(get_neighborhood(sector, nType))
+    return neighbors
+
+
+def get_things_each_direction(sector):
+    addon = NAddon(
+        # direction=nc.NAddon.DIR_EACH, #전 방향 탐색
+        tradeType=[NAddon.TRADE_DEAL, NAddon.TRADE_LEASE],  # 목표 거래 - 매매, 전세
+        estateType=[NAddon.ESTATE_APT, NAddon.ESTATE_OPST]  # 목표 매물 - 아파트, 오피스텔
+    )
+    things = []  # 매물 기록
+    for dirr in NAddon.DIR_EACH:  # 모든 방향 (남향 등등)
+        addon.dir = dirr  # 방향 조건 선택
+        things.extend(get_things(sector, addon))
+    return things
+
+
+def get_all_on_sector(sector: NSector):
+    things = get_things_each_direction(sector)
+    neighbors = get_all_neighbors(sector)
+    return (sector, things, neighbors)
